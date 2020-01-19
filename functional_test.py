e12a5e45d07c7630983826fbbb612cda688604b2
James Bradford <james@fishfacedesign.co.uk>
2020-01-19 14:13:56 +0000

    Spec first story/func. test in comments, implemented unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,40 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith hears about new fancy to-do list app and heads to the website
+		self.browser.get('http://localhost:8000')
+
+		# She sees the title and notices it mentions To-Do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish The Test!')
+
+		# She is invited to enter a to-do item straight away
+
+		# She types "Buy Peacock feathers" into the textbox
+
+		# When she hits enter the page updates, and now the page lists:
+		# "1. Buy Peacock feathers" as an item in to-do list.
+
+		# There is still a button to add more To-Do's so she enters:
+		# "Use peacock feathers to make a fly".
+
+		# Page updates again and lists her new item.
+
+		# Edith wonders if site will save her list over sessions
+		# she see's text explaining this on page.
+
+		# She revisits url, her list is restored.
+
+		# Happy, she closes the site and goes back to her life.
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+	
