1b2cfb09d65450bf3755be59b2056ebf01c9a52c
James Bradford <james@fishfacedesign.co.uk>
2020-01-19 16:37:25 +0000

    Show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -5,10 +5,12 @@ from django.http import HttpRequest
 from .views import home_page
 from .models import Item
 
+
 class HomePageTest(TestCase):
 
   def test_home_page_returns_correct_html(self):
     response = self.client.get('/')
+
     self.assertTemplateUsed(response, 'home.html')
 
   def test_can_save_a_POST_request(self):
@@ -20,12 +22,22 @@ class HomePageTest(TestCase):
 
   def test_redirects_after_POST(self):
     response = self.client.post('/', data={'item_text': 'A new list item'})
+
     self.assertEqual(response.status_code, 302)
     self.assertEqual(response['location'], '/')
 
   def test_only_saves_items_when_necessary(self):
     self.client.get('/')
     self.assertEqual(Item.objects.count(), 0)
+
+  def test_displays_all_list_items(self):
+    Item.objects.create(text='itemey 1')
+    Item.objects.create(text='itemey 2')
+
+    response = self.client.get('/')
+
+    self.assertIn('itemey 1', response.content.decode())
+    self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
