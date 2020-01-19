3a1e43692c3731b7bdd5b291df7850e3e97d85d4
James Bradford <james@fishfacedesign.co.uk>
2020-01-19 15:59:11 +0000

    Refactor functional test to remove duplicate checks of item table

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table =  self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith hears about new fancy to-do list app and heads to the website
 		self.browser.get('http://localhost:8000')
@@ -33,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
 		# "1. Buy Peacock feathers" as an item in to-do list.
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table =  self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a button to add more To-Do's so she enters:
 		# "Use peacock feathers to make a fly".
@@ -46,10 +48,8 @@ class NewVisitorTest(unittest.TestCase):
 		time.sleep(1)
 
 		# Page updates again and lists her new item.
-		table =  self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith wonders if site will save her list over sessions
 		# she see's text explaining this on page.
